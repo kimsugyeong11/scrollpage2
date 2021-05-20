6f8bdd7bfa957c8b7ab6d17a00dfe3102fdd46dc
kimsugyeong11 <82920744+kimsugyeong11@users.noreply.github.com>
2021-05-20 09:52:16 +0900

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(function(){
+  const addTop = 150;
+  //script 입력영역
+  $("nav ul li a,.back_to_top a,a.button,.footer_top a").click(function(){
+    console.log($(this.hash));
+    console.log($(this.hash).offset().top);
+    let thisElem = $(this.hash);
+    let offsetElem = thisElem.offset();
+    $("html,body").animate({scrollTop:offsetElem.top},1500)
+  });
+  //scroll 상단 이동버튼 노출/비노출
+  $(window).scroll(function(){
+    if( $(this).scrollTop() == 0 ){
+      $(".back_to_top").removeClass("on");
+    }else{
+      $(".back_to_top").addClass("on");
+    }
+  });
+  //section offset top 값으로 class 추가
+  let wHeight = $(window).innerHeight();
+  //console.log("window height:"+wHeight);
+  $(window).scroll(function(){
+    let thisScrollTop = $(this).scrollTop();
+    $("section").each(function(){
+      let thisOffset = $(this).offset();
+      if( thisOffset.top <= thisScrollTop + addTop && thisScrollTop < thisOffset.top + wHeight ){
+        $(this).addClass("active");
+      }
+    });
+  });
+});
